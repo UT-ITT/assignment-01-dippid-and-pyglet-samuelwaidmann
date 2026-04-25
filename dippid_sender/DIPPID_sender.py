92126bc3d4b010dbe8c3b7f915218f35d05f3f5f
samuelwaidmann <samuel.waidmann@student.uni-tuebingen.de>
2026-04-25 13:52:41 +0200

    create accelerometer simulator

--- dippid_sender/DIPPID_sender.py
+++ dippid_sender/DIPPID_sender.py
@@ -1,6 +1,45 @@
 import socket
 import time
-import numpy as np
+import math
+import json
 
 
-x = np.array(1)
+IP = '127.0.0.1'
+PORT = 5700
+DT = 2 # update rate
+
+
+class AccelerometerSimulator:
+    """Accelerometer simulator using simple sine waves."""
+
+    def __init__(self):
+        self.start_time = time.time()
+
+    def get_values(self):
+        t = time.time() - self.start_time
+
+        return {
+            'x': math.sin(2 * math.pi * 0.6 * t),
+            'y': math.sin(2 * math.pi * 0.9 * t),
+            'z': math.sin(2 * math.pi * 1.2 * t),
+        }
+
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    accel = AccelerometerSimulator()
+
+    print(f'Sending accelerometer data to {IP}:{PORT}')
+
+    while True:
+        values = accel.get_values()
+        message = json.dumps({'accelerometer': values})
+
+        print(message)
+        sock.sendto(message.encode(), (IP, PORT))
+
+        time.sleep(DT)
+
+
+if __name__ == "__main__":
+    main()
